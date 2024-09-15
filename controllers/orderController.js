4fa5b1f5359894a056f01476250a02695ad444b8
Sourav Jaiswal <jaiswalsourav5@gmail.com>
2024-09-15 21:55:48 +0530

    update port

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -11,7 +11,7 @@ const stripe = new Stripe(process.env.STRIPE_SECRET_KEY)
 // placing user order from frontend
 const placeOrder = async (req,res) => {
 
-    const frontend_url = process.env.FRONT_URL;
+    const frontend_url = "http://localhost:10000";
 
     try {
         const newOrder = new orderModel({
